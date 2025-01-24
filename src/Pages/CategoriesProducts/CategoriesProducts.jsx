9c05b919a073ad159f96896770e7c39f43ec2eca
Naimul02 <naimulislum39@gmail.com>
2025-01-23 22:50:04 -0800

    category products problem has been solved

--- src/Pages/CategoriesProducts/CategoriesProducts.jsx
+++ src/Pages/CategoriesProducts/CategoriesProducts.jsx
@@ -6,17 +6,22 @@ import MilkProducts from '../Home/MilkProducts/MilkProducts';
 
 const CategoriesProducts = () => {
     const {category} = useParams();
-    const {data} = useQuery({
-        queryKey : ['CategoriesProducts'], 
+    console.log("category : " , category)
+    const {data } = useQuery({
+        queryKey : ['CategoriesProducts' , category], 
         queryFn : async() => {
             const res = await axios.get(`http://localhost:5000/categoryProducts/${category}`)
             console.log(res.data);
             const result = res.data;
-            return result
+            console.log("result " , result)
+            return result;
+            
         }
     })
     return (
-        <div className="container mx-auto">
+        <div className='bg-slate-100'>
+
+        <div className="container px-auto pt-40 pb-16">
             <h1 className="text-xl font-semibold">Category</h1>
             
             <div className="grid grid-cols-5 gap-3">
@@ -24,6 +29,7 @@ const CategoriesProducts = () => {
                     data?.map(product => <MilkProducts product={product}></MilkProducts>)
                 }
             </div>
+        </div>
         </div>
     );
 };
