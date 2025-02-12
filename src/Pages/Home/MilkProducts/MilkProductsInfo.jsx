9708c4167fa048f7656f6b822d1d2db609efe415
Naimul02 <naimulislum39@gmail.com>
2025-02-12 11:29:45 -0800

    responsive delivery section

--- src/Pages/Home/MilkProducts/MilkProductsInfo.jsx
+++ src/Pages/Home/MilkProducts/MilkProductsInfo.jsx
@@ -32,10 +32,10 @@ const MilkProductsInfo = () => {
     <div className="bg-slate-100 pb-10">
       <div className="max-w-3xl mx-auto text-center space-y-2 pt-10 mb-10">
       <h1 className="text-xl font-bold">Products for Daily Shopping</h1>
-      <p className="text-gray-600">See all our popular products in this week. You can choose your daily needs products from this list and get some special offer with free shipping.</p>
+      <p className="text-gray-600 px-4 lg:px-0">See all our popular products in this week. You can choose your daily needs products from this list and get some special offer with free shipping.</p>
       </div>
 
-      <div className="grid lg:grid-cols-5 grid-cols-1 gap-4 mx-4">
+      <div className="grid grid-cols-2 lg:grid-cols-5 gap-2 lg:gap-4 lg:mx-4 mx-2">
 
 
         {
